208c03297015d40346137a61fed68172e2100df0
Undercroft0 <seekndestroy0501@gmail.com>
2022-02-25 13:38:57 +0800

    Main class nemsen

--- F.CS311/src/Queue.java
+++ F.CS311/src/Queue.java
@@ -79,52 +79,5 @@ public class Queue {
     }
   }
 
-  public static void main(String[] args) {
-	  
-	  Scanner scanner = new Scanner(System.in);
-    Queue q = new Queue();
-    
-    boolean swtch = true;
- try { 
-    while(swtch) {
-
-    System.out.println("Uildlee songono uu:\n" +
-    		"1. Enqueue\n"
-			+ "2. Dequeue\n"
-			+ "3. Peek\n"
-			+ "4. Display\n"
-			+ "5. isEmpty\n"
-			+ "6. isFull\n"
-			+ "7. Program shutdown");
-    switch(scanner.nextInt()) {
-    case 1:
-    	System.out.println("Queue-д нэмэх элементээ оруулна уу: ");
-		q.enQueue(scanner.nextInt()); 
-		break;
-	case 2:
-		q.deQueue(); 
-		break;
-	case 3:
-		q.peek();
-		break;
-	case 4:
-		System.out.println("Queue-гийн бүх элементийг харах: ");
-		q.display();
-		break;
-	case 5:
-		System.out.println("Queue хоосон уу = " + q.isEmpty());
-		
-		break;
-	case 6:
-		System.out.println("Queue дүүрсэн үү = " + q.isFull());
-		break;
-	case 7:
-		swtch = false;
-		break;
-    }
-    }
- } catch (Exception e) {
-	 System.out.println("Алдаа гарлаа!");
- }
-  }
+ 
 }
